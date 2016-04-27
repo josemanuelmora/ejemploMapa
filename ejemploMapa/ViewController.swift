3ec6211babef20cca4b3d37948bab33b4998c543
José Manuel Mora <josema81@hotmail.com>
2016-04-27 12:42:35 -0500

    Initial Commit

---
+++ ejemploMapa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejemploMapa
+//
+//  Created by José Manuel Mora on 27/04/16.
+//  Copyright © 2016 Morasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
